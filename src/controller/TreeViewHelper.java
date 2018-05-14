7771f3328004054b184a3f434bf6d4e2e10fba3b
Rossolini Andrea <andrea.rossolini2@studio.unibo.it>
2018-05-15 00:02:32 +0200

    new class TreeViewHelper in controller package

--- src/controller/TreeViewHelper.java
+++ src/controller/TreeViewHelper.java
@@ -1,31 +1,51 @@
 package controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import javafx.scene.control.TreeItem;
 import model.*;
 import utilities.enumerations.Color;
 
+/**
+ * This class creates treeItem Objects that will be used in buying and selling dialogs
+ * and mortgage.
+ * 
+ * @author Rossolini Andrea
+ *
+ */
 public class TreeViewHelper {
 	private final Map<Color, List<Obtainable>> playersProperties;
-	private List<TreeItem<String>> product;
 
 	public TreeViewHelper(final Map<Color, List<Obtainable>> playersProperties) {
 		this.playersProperties = new HashMap<>();
 		this.playersProperties.putAll(playersProperties);
-		product = new ArrayList<>();
 	}
 
 	public List<TreeItem<String>> getProducts() {
-		this.playersProperties.forEach((k, v) -> {
-			TreeItem<String> fatherItem = new TreeItem<String>(k.getName());
-			this.playersProperties.get(k).forEach(e -> {
-				fatherItem.getChildren().add(new TreeItem<String>(e.getNameOf()));
-			});
-			this.product.add(fatherItem);
-		});
+//		this.playersProperties.forEach((k, v) -> {
+//			TreeItem<String> fatherItem = new TreeItem<String>(k.getName());
+//			this.playersProperties.get(k).forEach(e -> {
+//				fatherItem.getChildren().add(new TreeItem<String>(e.getNameOf()));
+//			});
+//			this.product.add(fatherItem);
+//		});
+//		return product;
+		
+		List<TreeItem<String>> product =
+			    this.playersProperties.entrySet().stream().map(
+			    t -> {
+			          TreeItem<String>father = new TreeItem<>(t.getKey().getName());
+			          List<TreeItem<String>> children = t.getValue()
+			                .stream()
+			                .map(u -> u.getNameOf())
+			                .map(TreeItem::new)
+			                .collect(Collectors.toList());
+			         father.getChildren().addAll(children);
+			         return father;
+			     }
+			    ).collect(Collectors.toList());
 		return product;
 	}
 
---
+++ src/view/Dialog.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package view;
+
+/**
+ * Class for dialogs, other classes can extends this class to initiate the stage
+ * 
+ * @author Rossolini Andrea
+ *
+ */
+public class Dialog {
+
+}
