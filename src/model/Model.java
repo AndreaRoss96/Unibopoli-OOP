517daa723a92db32e419c1b16779f0eb8bef4759
andrea.rossolini2@studio.unibo.it <andrea.rossolini2@studio.unibo.it>
2018-08-10 15:52:52 +0200

    Review of ModelImpl

--- src/model/Model.java
+++ src/model/Model.java
@@ -4,20 +4,40 @@ import java.util.List;
 import java.util.Set;
 
 import model.player.Player;
+import model.player.PlayerInfo;
 import model.tiles.Tile;
 import utilities.Pair;
 
 public interface Model {
 
-	Player getCurrentPlayer();
+	/**
+	 * @return the player who is currently playing.
+	 */
+	PlayerInfo getCurrentPlayer();
 	
+	/**
+	 * @return the list of all player in game.
+	 */
 	List<Player> getPlayers();
+	
+	/**
+	 * remove a player out of the game.
+	 */
+	void removePlayer(PlayerInfo player);
 	
 	void startGame();
 	
+	/**
+	 * Saves the game.
+	 */
 	void saveGame();
 	
 	void loadGame();
+	
+	/**
+	 * Quits the game.
+	 */
+	void endGame();
 	
 	Pair<Integer, Integer> exitDice();
 	
--- src/model/ModelImpl.java
+++ src/model/ModelImpl.java
@@ -3,13 +3,29 @@ package model;
 import java.util.List;
 import java.util.Set;
 
+import model.player.Player;
+import model.player.PlayerInfo;
+import model.tiles.Obtainable;
 import model.tiles.Tile;
 import utilities.Pair;
 
 public class ModelImpl implements Model{
+	private final Board board;
+	private final List<Player> players;
+	private final Set<Obtainable> properties;
+	private final Player currentPlayer;
+	//imprevisti e probabilità
+	
+	public ModelImpl(final Board board, final List<Player> players, final Set<Obtainable> properties, final Player currentPlayer) {
+		this.board = board;
+		this.players = players;
+		this.properties = properties;
+		this.currentPlayer = currentPlayer;
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
-	public Player getCurrentPlayer() {
+	public Player getCurrentPlayer1() {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -28,8 +44,7 @@ public class ModelImpl implements Model{
 
 	@Override
 	public void saveGame() {
-		// TODO Auto-generated method stub
-		
+		ResourceManager.getInstance().saveOnFile(CareMementoTaker.getMementoInstance().getMemento());
 	}
 
 	@Override
@@ -48,6 +63,30 @@ public class ModelImpl implements Model{
 	public Set<Tile> getBoard() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public void addPlayer(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PlayerInfo getCurrentPlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removePlayer(PlayerInfo player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endGame() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
